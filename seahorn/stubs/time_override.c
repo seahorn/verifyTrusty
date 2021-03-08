9de6b6ec64e6f0245b977adb5384abd9f49b468e
Daniel Zhou <danzhou1810@gmail.com>
2021-03-08 18:27:13 -0500

    feat(job): main function only in harness with CMake tricks

--- seahorn/stubs/time_override.c
+++ seahorn/stubs/time_override.c
@@ -1,6 +1,4 @@
 #include "nondet.h"
 #include <time.h>
 
-int trusty_gettime(clockid_t clock_id, int64_t* time) {
-    return nd_int();
-}
+int trusty_gettime(clockid_t clock_id, int64_t *time) { return nd_int(); }
