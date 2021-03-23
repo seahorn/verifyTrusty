3437bb033436fd3b673fe751641d0ccb7974f3b2
yusen <y256su@uwaterloo.ca>
2021-03-23 03:32:49 +0000

    refactor(repo): remove unused files

--- seahorn/trusty_common/stubs/new_delete_override.cpp
+++ seahorn/trusty_common/stubs/new_delete_override.cpp
@@ -2,14 +2,16 @@
 #include <cstdlib>
 using namespace std;
 
-void *operator new[](size_t sz) noexcept {
-    if (sz == 0) return nullptr;
-    if (void *ptr = std::malloc(sz))
-        return ptr;
+void *operator new(size_t sz) noexcept {
+  if (sz == 0)
     return nullptr;
+  if (void *ptr = std::malloc(sz))
+    return ptr;
+  return nullptr;
 }
 
-void operator delete(void* ptr) noexcept
-{
-    std::free(ptr);
-}
+void *operator new[](size_t sz) noexcept { return ::operator new(sz); }
+
+void operator delete(void *ptr) noexcept { std::free(ptr); }
+
+void operator delete[](void *ptr) noexcept { ::operator delete(ptr); }
