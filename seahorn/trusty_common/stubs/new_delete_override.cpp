2269a8c1d8104d6d1125555bc572eeec5830d212
yusen <y256su@uwaterloo.ca>
2021-04-08 02:06:07 +0000

    feat(lib): add more functions for new delete override

--- seahorn/trusty_common/stubs/new_delete_override.cpp
+++ seahorn/trusty_common/stubs/new_delete_override.cpp
@@ -1,5 +1,6 @@
 
 #include <cstdlib>
+#include <new> 
 using namespace std;
 
 void *operator new(size_t sz) noexcept {
@@ -12,6 +13,18 @@ void *operator new(size_t sz) noexcept {
 
 void *operator new[](size_t sz) noexcept { return ::operator new(sz); }
 
+void *operator new(size_t sz, const std::nothrow_t&) noexcept {
+  return ::operator new(sz);
+}
+
+void *operator new[](size_t sz, const std::nothrow_t&) noexcept {
+  return ::operator new(sz);
+}
+
 void operator delete(void *ptr) noexcept { std::free(ptr); }
 
 void operator delete[](void *ptr) noexcept { ::operator delete(ptr); }
+
+void operator delete(void *ptr, const std::nothrow_t&) noexcept { ::operator delete(ptr); }
+
+void operator delete[](void *ptr, const std::nothrow_t&) noexcept { ::operator delete(ptr); }
