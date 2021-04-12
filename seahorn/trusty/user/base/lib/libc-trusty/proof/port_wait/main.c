c9e44c4f3b9a932e93d1a602fc60edf57d1e875a
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-04-12 17:20:08 -0400

    feat: additional tests for ipcs model

--- seahorn/trusty/user/base/lib/libc-trusty/proof/port_wait/main.c
+++ seahorn/trusty/user/base/lib/libc-trusty/proof/port_wait/main.c
@@ -10,19 +10,43 @@ int main(void) {
   // expect non-secure handle
   sassert(port == 2);
 
-  uevent_t event;
+  uevent_t event = {.handle = INVALID_IPC_HANDLE, .event = 0, .cookie = NULL};
 
-  event.handle = INVALID_IPC_HANDLE;
-  event.event = 0;
-  event.cookie = NULL;
-
-  handle_t rc = wait(port, &event, 0);
+  int rc = wait(port, &event, 0);
   sassert(rc <= NO_ERROR);
   if (rc == NO_ERROR) {
     /* got an event */
     sassert(event.handle == port);
     // No cookie set
     sassert(!event.cookie);
+  }
+
+  uuid_t peer_uuid;
+  handle_t chan = accept(port, &peer_uuid);
+  assume(chan != INVALID_IPC_HANDLE);
+  sassert(chan == 16);
+
+  rc = wait(chan, &event, 0);
+  sassert(rc <= NO_ERROR);
+  if (rc == NO_ERROR) {
+    if (event.event & IPC_HANDLE_POLL_MSG) {
+      struct ipc_msg_info msg_info;
+      rc = get_msg(chan, &msg_info);
+      assume(rc == NO_ERROR);
+      sassert(msg_info.id != 0);
+
+      char buf[4096];
+      struct iovec iov = {.iov_base = buf, .iov_len = sizeof(buf)};
+      struct ipc_msg msg = {.num_iov = 1, .iov = &iov};
+      rc = read_msg(chan, msg_info.id, 0, &msg);
+      assume(rc >= NO_ERROR);
+      sassert(rc <= 4096);
+
+      rc = put_msg(chan, msg_info.id);
+      assume(rc == NO_ERROR);
+      rc = read_msg(chan, msg_info.id, 0, &msg);
+      sassert(rc < NO_ERROR);
+    }
   }
 
   return 0;
