0e0f640cc47b28c504860a37eb5523bbf9f97acd
yusen <y256su@uwaterloo.ca>
2021-06-04 15:43:09 +0000

    feat(lib): add new msg handler to cover sync_ipc_send_msg

--- seahorn/storage/include/sea_ipc_helper.h
+++ seahorn/storage/include/sea_ipc_helper.h
@@ -12,3 +12,8 @@ void sea_dispatch_event(const uevent_t* ev);
  * Initialize a port context by given a channel context handler
  */
 struct ipc_port_context* create_port_context(void);
+
+
+struct ipc_channel_context *
+sea_sync_channel_connect(struct ipc_port_context *parent_ctx,
+                    const uuid_t *peer_uuid, handle_t chan_handle);
